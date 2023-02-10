6ac5ff84ddf8bf648058067ff3c2a7ee45d244d5
AleksDerivolkov <124775973+AleksDerivolkov@users.noreply.github.com>
2023-02-10 12:21:50 +0200

    Coding
    Tova koeto kodirah v visualstudio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aleksandr_9a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 3 words!");
+            
+        }
+    }
+}
